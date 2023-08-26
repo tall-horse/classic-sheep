5341fee87804ac776c74935eaa5b66e570f6e138
tall-horse <54848630+tall-horse@users.noreply.github.com>
2023-08-26 21:00:32 +0200

    Comparing the Old & the New Part 3

--- ClassicSheep/Assets/SpawnParallel.cs
+++ ClassicSheep/Assets/SpawnParallel.cs
@@ -7,29 +7,48 @@ using Unity.Jobs;
 public class SpawnParallel : MonoBehaviour
 {
   public GameObject sheepPrefab;
-  GameObject[] allSheep;
+  Transform[] allSheepTransform;
+  struct MoveJob : IJobParallelForTransform
+  {
+    public void Execute(int index, TransformAccess transform)
+    {
+      transform.position += 0.1f * (transform.rotation * new Vector3(0, 0, 1));
+      if(transform.position.z > 50)
+      {
+        transform.position = new Vector3(transform.position.x, 0, -50);
+      }
+    }
+  }
+  MoveJob moveJob;
+  JobHandle moveHandle;
+  TransformAccessArray transforms;
   const int numSheep = 15000;
   // Start is called before the first frame update
   void Start()
   {
-    allSheep = new GameObject[numSheep];
+    allSheepTransform = new Transform[numSheep];
     for (int i = 0; i < numSheep; i++)
     {
       Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
-      allSheep[i] = Instantiate(sheepPrefab, pos, Quaternion.identity);
+      GameObject sheep = Instantiate(sheepPrefab, pos, Quaternion.identity);
+      allSheepTransform[i] = sheep.transform;
     }
+    transforms = new TransformAccessArray(allSheepTransform);
   }
 
   // Update is called once per frame
   void Update()
   {
-    for (int i = 0; i < numSheep; i++)
-    {
-      allSheep[i].transform.Translate(0, 0, 0.1f);
-      if (allSheep[i].transform.position.z > 50)
-      {
-        allSheep[i].transform.position = new Vector3(allSheep[i].transform.position.x, 0, -50);
-      }
-    }
+    moveJob = new MoveJob {};
+    moveHandle = moveJob.Schedule(transforms);
+  }
+
+  private void LateUpdate() {
+    moveHandle.Complete();
+  }
+
+  void OnDestroy()
+  {
+    transforms.Dispose();  
   }
 }
